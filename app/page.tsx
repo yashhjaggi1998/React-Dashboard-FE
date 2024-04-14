e8db5f804828da36e0bd9f22f5113048b4f9f23e
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-04-13 22:16:18 -0700

    Fixing Quick Suggestion Alignment

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import {
 import {
     Card,
     CardDescription,
+    CardFooter,
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
@@ -483,27 +484,26 @@ export default function Home() {
                                 return segment.Rating >= 4.0 ? (
                                     <Card className="border rounded-lg col-span-1" key={index}>
                                         <CardHeader className="p-0"></CardHeader>
-                                        <CardDescription className="px-3 pt-4 pb-0">
-                                            <div className="flex items-center align-items-center">
-                                                <span className=" relative h-9 w-6 text-green-700">
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        width="20"
-                                                        height="20"
-                                                        fill="currentColor"
-                                                        className="bi bi-shield-fill-check"
-                                                        viewBox="0 0 16 16"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793z"
-                                                        />
-                                                    </svg>
-                                                </span>
-                                                <p className="text-lg font-bold">{segment.Skill}</p>
-                                            </div>
-                                            <p className="text-md text-muted-foreground">{segment.QuickSuggestion}</p>
+                                        <CardDescription className="flex items-center align-items-center relative text-green-700 px-3 pt-4 pb-0">
+                                                
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="20"
+                                                height="20"
+                                                fill="currentColor"
+                                                className="bi bi-shield-fill-check"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793z"
+                                                />
+                                            </svg>
+                                                <span className="text-lg font-bold">{segment.Skill}</span>
                                         </CardDescription>
+                                        <CardFooter>
+                                            <p className="text-md text-muted-foreground">{segment.QuickSuggestion}</p>
+                                        </CardFooter>
                                     </Card>
                                 ) : null;
                             })}
