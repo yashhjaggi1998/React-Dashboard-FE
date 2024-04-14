899f7ca7d20f9808c05d6cfa1b3f2d21cf596c43
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-04-13 22:20:54 -0700

    Quick Suggestion Card Fix

--- app/page.tsx
+++ app/page.tsx
@@ -484,7 +484,7 @@ export default function Home() {
                                 return segment.Rating >= 4.0 ? (
                                     <Card className="border rounded-lg col-span-1" key={index}>
                                         <CardHeader className="p-0"></CardHeader>
-                                        <CardDescription className="flex items-center align-items-center relative text-green-700 px-3 pt-4 pb-0">
+                                        <CardDescription className="flex items-center align-items-center relative text-green-700 px-3 pt-3 pb-0">
                                                 
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
@@ -499,7 +499,7 @@ export default function Home() {
                                                     d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793z"
                                                 />
                                             </svg>
-                                                <span className="text-lg font-bold">{segment.Skill}</span>
+                                            <span className="text-lg text-muted-foreground font-bold ms-2">{segment.Skill}</span>
                                         </CardDescription>
                                         <CardFooter>
                                             <p className="text-md text-muted-foreground">{segment.QuickSuggestion}</p>
@@ -521,7 +521,7 @@ export default function Home() {
                                                         <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m-.55 8.502L7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0M8.002 12a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
                                                     </svg>
                                                 </span>
-                                                <p className="text-lg font-bold">{segment.Skill}</p>
+                                                <p className="text-lg font-bold ">{segment.Skill}</p>
                                             </div>
                                             <p className="text-md text-muted-foreground">{segment.QuickSuggestion}</p>
                                         </CardDescription>
