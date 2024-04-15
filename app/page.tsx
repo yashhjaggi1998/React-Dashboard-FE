091b0503b7581b9cd91adde233ac56ddb4c59bf2
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-04-15 17:58:49 -0400

    Transcript Play button

--- app/page.tsx
+++ app/page.tsx
@@ -642,20 +642,20 @@ export default function Home() {
                                                             {  isAudioReady && audioSrc ? ( !isAudioPlaying ? 
                                                                     (
                                                                         <span
-                                                                            className="flex rounded-full bg-slate-600 hover:bg-black h-10 w-10 items-center justify-center border-4 border-muted cursor-pointer"
+                                                                            className="flex rounded-full bg-slate-600 hover:bg-black h-12 w-12 items-center justify-center border-4 border-muted cursor-pointer"
                                                                             onClick={() => audioRef.current?.play()}
                                                                         >
-                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-fill text-light" viewBox="0 0 16 16">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-play-fill text-light" viewBox="0 0 16 16">
                                                                                 <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
                                                                             </svg>
                                                                         </span>
                                                                     ) : 
                                                                     (
                                                                         <span 
-                                                                            className="flex rounded-full cursor-pointer bg-slate-600 hover:bg-black h-10 w-10 items-center justify-center border-4 border-muted"
+                                                                            className="flex rounded-full cursor-pointer bg-slate-600 hover:bg-black h-12 w-12 items-center justify-center border-4 border-muted"
                                                                             onClick={() => audioRef.current?.pause()}
                                                                         >
-                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pause-fill text-light" viewBox="0 0 16 16">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pause-fill text-light" viewBox="0 0 16 16">
                                                                                 <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
                                                                             </svg>
                                                                         </span>
